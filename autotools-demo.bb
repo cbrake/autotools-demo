248e54472c25b2e47c3423c63793ade032b2d0ae
Cliff Brake <cbrake@bec-systems.com>
2016-07-05 11:17:26 -0400

    autotools demo

---
+++ autotools-demo.bb
@@ -0,0 +1,7 @@
+# recipe to build the autotools-demo in the working tree
+
+inherit srctree autotools gitver
+
+PV = "${GITVER}"
+
+
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	printf("autotools demo\n");
+
+	for (i=0; i < 10; i++) {
+		printf("Hello World i = %i\n", i);
+	}
+
+}
+
